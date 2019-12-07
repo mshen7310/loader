8c9df1620d2d2b56b4ca9eeed98d6f212d80845f
feng.shen <mshen@sparkpad.com>
2019-12-08 02:31:54 +0800

    add module

--- test/test.js
+++ test/test.js
@@ -44,15 +44,14 @@ describe(`Adapter Testing ${__filename}`, function(){
 		const p = plugin.module({}, 'test/test.dir');
 		assert.ok(match(p, {test:match.function}));
 		const u = p.test(undefined, 'linkaa.js', 'testing');
-		console.log(u);
-		assert.ok(match(u, {
+		assert.ok(match(u, match.funobj({
 			a:['a.testing.json','a.testing.js'],
 			b:['b.testing.js','b.testing.json'],
 			c:['c.js','c.json']
-		}));
-		assert.throws(plugin.module(null, 'hello'));
-		assert.throws(plugin.module({}));
-		assert.throws(plugin.module());
+		})));
+		assert.throws(()=>plugin.module(null, 'hello'));
+		assert.throws(()=>plugin.module({}));
+		assert.throws(()=>plugin.module());
 	});
 	it('should load symbol link', function(){
 		const p = plugin('test/test.dir/linkaa', 'testing');
